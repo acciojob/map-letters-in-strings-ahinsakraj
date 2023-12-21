c5db8b6ca81c2218dc7f647d00af57ae6866fd99
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 17:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function mapLatters(str) {
+	let map={};
+	for(let i=0;i<str.length;i++){
+		
+		if(map[str[i]]){
+			map[str[i]].push(i);
+		}
+		else{
+			map[str[i]]=[i];
+		}
+	}
+	return map;
+}
+
+const str=prompt();
+alert(JSON.stringify(mapLatters(str)));
